39ee66d58df1bff3fd2afe64b38b282532a7f973
Gabriel Berdejo <i201812434@cibertec.edu.pe>
2022-05-03 16:07:04 -0500

    feat: agregando jest

---
+++ test/index.spec.ts
@@ -0,0 +1,12 @@
+// import request from 'supertest';
+// import app from '../src/app';
+
+describe('GET /tasks', () => {
+  //   test('should respond with a 200 sratus code', async () => {
+  //     const response = await request(app).get('/tasks').send();
+  //     expect(response.statusCode).toBe(200);
+  //   });
+  test('qweqweqwe', () => {
+    expect(1).toBe(1);
+  });
+});
